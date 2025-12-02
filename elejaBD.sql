555e6caecfc712e0ecfb39e05fc13191b279f7f4
JOAO <90044288+joaoluis2211@users.noreply.github.com>
2025-12-02 18:10:25 -0300

    apresentação suporte

--- elejaBD.sql
+++ elejaBD.sql
@@ -66,7 +66,7 @@ constraint fk_eleicao_voto foreign key (ideleicao) references eleicao (ideleicao
 constraint fk_candidato_voto foreign key (idcandidato) references candidato (idcandidato)
 );
 
-INSERT INTO usuario (email, senha, tipo) values ('administrador@gmail.com', '123', 'administrador');
+INSERT INTO usuario (email, senha, tipo) values ('elejaadmin@fatec.sp.gov.br', '123456', 'administrador');
 
 INSERT INTO turma (semestre, curso) values ('1', 'Desenvolvimento de Software Multiplataforma');
 INSERT INTO turma (semestre, curso) values ('2', 'Desenvolvimento de Software Multiplataforma');
@@ -90,7 +90,100 @@ INSERT INTO turma (semestre, curso) values ('5', 'Gestão Empresarial');
 INSERT INTO turma (semestre, curso) values ('6', 'Gestão Empresarial');
 
 
+INSERT INTO aluno (nome, idturma, ra) values ('Paulo Lopes', '2', '25846321');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('paulo.lopes@fatec.sp.gov.br', '123456', 'aluno', 1);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Andre Silva', '2', '25846322');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('andre.silva@fatec.sp.gov.br', '123456', 'aluno', 2);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Pedro Lima', '2', '25846323');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('pedro.lima@fatec.sp.gov.br', '123456', 'aluno', 3);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Jessica Lopes', '2', '25846324');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('jessica.lopes@fatec.sp.gov.br', '123456', 'aluno', 4);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Andressa Fagundes', '2', '25846325');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('andressa.fagundes@fatec.sp.gov.br', '123456', 'aluno', 5);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Amanda Lopes', '2', '25846326');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('amanda.lopes@fatec.sp.gov.br', '123456', 'aluno', 6);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Lucas Silva', '2', '25846327');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('lucas.silva@fatec.sp.gov.br', '123456', 'aluno', 7);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Carlos Ferreira', '2', '25846328');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('carlos.ferreira@fatec.sp.gov.br', '123456', 'aluno', 8);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Leandro Silva', '2', '25846329');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('leandro.silva@fatec.sp.gov.br', '123456', 'aluno', 9);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Jenifer Lopes', '2', '25846330');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('jenifer.lopes@fatec.sp.gov.br', '123456', 'aluno', 10);
+
+insert into eleicao (dataInicioCandidatura, dataFimCandidatura, dataInicioVotacao, dataFimVotacao, idturma, status) values ('2025-11-11', '2025-11-20', '2025-12-01', '2025-12-20', 2, 'VOTACAO');
+
+insert into candidato (idaluno, ideleicao, qtdVotos) values (1, 1, 0);
+insert into candidato (idaluno, ideleicao, qtdVotos) values (2, 1, 2);
+insert into candidato (idaluno, ideleicao, qtdVotos) values (3, 1, 7);
 
 
+insert into voto (idaluno, idcandidato, ideleicao) values (2, 2, 1);
+insert into voto (idaluno, idcandidato, ideleicao) values (3, 2, 1);
+insert into voto (idaluno, idcandidato, ideleicao) values (4, 3, 1);
+insert into voto (idaluno, idcandidato, ideleicao) values (5, 3, 1);
+insert into voto (idaluno, idcandidato, ideleicao) values (6, 3, 1);
+insert into voto (idaluno, idcandidato, ideleicao) values (7, 3, 1);
+insert into voto (idaluno, idcandidato, ideleicao) values (8, 3, 1);
+insert into voto (idaluno, idcandidato, ideleicao) values (9, 3, 1);
+insert into voto (idaluno, idcandidato, ideleicao) values (10, 3, 1);
 
 
+INSERT INTO aluno (nome, idturma, ra) values ('Paulo Silva', '3', '12547864');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('paulo.silva@fatec.sp.gov.br', '123456', 'aluno', 11);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Andre Lopes', '3', '12478532');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('andre.lopes@fatec.sp.gov.br', '123456', 'aluno', 12);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Pedro Ferreira', '3', '54698521');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('pedro.ferreira@fatec.sp.gov.br', '123456', 'aluno', 13);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Jessica Silva', '3', '25463215');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('jessica.silva@fatec.sp.gov.br', '123456', 'aluno', 14);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Andressa Lopes', '3', '25487691');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('andressa.lopes@fatec.sp.gov.br', '123456', 'aluno', 15);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Amanda Lima', '3', '12458691');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('amanda.lima@fatec.sp.gov.br', '123456', 'aluno', 16);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Lucas Ferreira', '3', '36589214');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('lucas.ferreira@fatec.sp.gov.br', '123456', 'aluno', 17);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Carlos Oliveira', '3', '15487962');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('carlos.oliveira@fatec.sp.gov.br', '123456', 'aluno', 18);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Leandro Oliveira', '3', '65894587');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('leandro.oliveira@fatec.sp.gov.br', '123456', 'aluno', 19);
+
+INSERT INTO aluno (nome, idturma, ra) values ('Jenifer Silva', '3', '14785963');
+INSERT INTO usuario (email, senha, tipo, idaluno) values ('jenifer.silva@fatec.sp.gov.br', '123456', 'aluno', 20);
+
+insert into eleicao (dataInicioCandidatura, dataFimCandidatura, dataInicioVotacao, dataFimVotacao, idturma, status) values ('2025-11-11', '2025-11-20', '2025-11-25', '2025-12-01', 3, 'ENCERRADA');
+
+insert into candidato (idaluno, ideleicao, qtdVotos) values (11, 2, 5);
+insert into candidato (idaluno, ideleicao, qtdVotos) values (12, 2, 3);
+insert into candidato (idaluno, ideleicao, qtdVotos) values (13, 2, 2);
+
+
+insert into voto (idaluno, idcandidato, ideleicao) values (11, 4, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (12, 4, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (13, 4, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (14, 4, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (15, 4, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (16, 5, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (17, 5, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (18, 5, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (19, 6, 2);
+insert into voto (idaluno, idcandidato, ideleicao) values (20, 6, 2);
+
+insert into ata (representante, vice, ideleicao, idturma, data) values ('Paulo Silva', 'Andre Lopes', 2, 3, '2025-12-01');
